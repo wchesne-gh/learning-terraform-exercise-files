4f445dc9a236daf27097d56f14d46c91053b42ab
wchesne-gh <wchesne+github@wgu.edu>
2026-07-29 19:56:18 -0400

    New main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
+
+variable "my_ip_cidr" {
+  description = "Your public IP address in CIDR notation for SSH and Tomcat access"
+  type        = string
+}
+
+variable "tomcat_version" {
+  description = "Apache Tomcat version to install"
+  type        = string
+  default     = "10.1.24"
+}
+
+variable "tomcat_major" {
+  description = "Apache Tomcat major release series"
+  type        = string
+  default     = "10"
+}
+
 data "aws_ami" "al2023" {
   most_recent = true
   owners      = ["amazon"]
@@ -8,16 +36,37 @@ data "aws_ami" "al2023" {
   }
 }
 
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name               = "tomcat-spot-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "tomcat-spot-ec2-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
 resource "aws_security_group" "tomcat_sg" {
   name        = "tomcat-spot-sg"
-  description = "Allow SSH and Tomcat access"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
-  }
+  description = "Allow Tomcat access"
 
   ingress {
     from_port   = 8080
@@ -43,9 +92,8 @@ resource "aws_spot_instance_request" "tomcat_spot" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.tomcat_sg.id]
-  key_name               = var.key_name
-
-  spot_type = "one-time"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
+  spot_type              = "one-time"
 
   user_data = <<-EOF
               #!/bin/bash
@@ -56,8 +104,8 @@ resource "aws_spot_instance_request" "tomcat_spot" {
 
               id tomcat &>/dev/null || useradd -r -m -U -d /opt/tomcat -s /sbin/nologin tomcat
 
-              TOMCAT_VERSION=10.1.24
-              TOMCAT_MAJOR=10
+              TOMCAT_VERSION=${var.tomcat_version}
+              TOMCAT_MAJOR=${var.tomcat_major}
               TOMCAT_URL="https://archive.apache.org/dist/tomcat/tomcat-${TOMCAT_MAJOR}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
 
               cd /tmp
