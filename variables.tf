627ad9e2e5befdf93536da6fe94a3b838b13b97a
wchesne-gh <wchesne+github@wgu.edu>
2026-07-29 19:42:53 -0400

    Update variables.tf
    Added Public IP  my_ip_cidr

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
 #variable "instance_type" {
 #  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
+#  default     = "t3.micro"
 #}
+variable "my_ip_cidr" {
+  description = "Your public IP address in CIDR notation for SSH and Tomcat access"
+  type        = string
+}
