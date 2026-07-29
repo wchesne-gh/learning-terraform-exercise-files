cb708cf93651e593c3daada55fe2ab1a917629bb
wchesne-gh <wchesne+github@wgu.edu>
2026-07-29 19:51:39 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.micro"
-#}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
+
 variable "my_ip_cidr" {
   description = "Your public IP address in CIDR notation for SSH and Tomcat access"
   type        = string
