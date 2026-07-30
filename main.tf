b21c83ca19cf5194c39dd1bcc2c513cf9e978ad1
wchesne-gh <wchesne+github@wgu.edu>
2026-07-29 21:18:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -75,6 +75,13 @@ resource "aws_security_group" "tomcat_sg" {
     cidr_blocks = [var.my_ip_cidr]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
   egress {
     from_port   = 0
     to_port     = 0
